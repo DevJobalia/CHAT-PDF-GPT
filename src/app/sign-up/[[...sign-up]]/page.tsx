33c7a6d6b231ca3a6dadc597526cd6184226b61b
Dev Jobalia <webdev.jobalia@gmail.com>
2024-02-05 20:18:50 +0530

    CLERK SERVER DOWN

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
